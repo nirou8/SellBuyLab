c5e9c6f28d03a3f15fb54cde2a8aecc86ca68602
Nadhir <khayati.nadhir@gmail.com>
2019-11-01 19:36:37 -0400

    skeleton of components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { AsideComponent } from './aside/aside.component';
+import { ContentComponent } from './content/content.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent,
+    AsideComponent,
+    ContentComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
