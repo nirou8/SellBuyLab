d84337145b4d11ddac7550d6533e50821f0acf9b
Nadhir <khayati.nadhir@gmail.com>
2019-11-13 19:21:26 -0500

    Lecture 10 ngFor

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+laptops = [
+  {name : "ASUS", price : "1400$", description : "this is an ASUS laptop"},
+{name : "Lenovo", price : "1200$", description : "this is a Lenovo laptop"},
+{name : "ACER", price : "1500$", description : "this is an ACER laptop"},
+{name : "DELL", price : "2000$", description : "this is a DELL laptop"},
+{name : "Samsung", price : "1800$", description : "this is a Samsung laptop"},
+{name : "HP", price : "1600$", description : "this is an HP laptop"},
+];
   constructor() { }
 
   ngOnInit() {
